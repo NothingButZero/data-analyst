e0b62473b980db634548715f197bf9e22f0f4a16
Jay Gelani <140696513+NothingButZero@users.noreply.github.com>
2026-06-07 17:36:55 +0530

    Fix SQL query to include product names in selection

--- Mysql/practice set - 1/task 7.sql
+++ Mysql/practice set - 1/task 7.sql
@@ -7,8 +7,12 @@ call table_order_items();
 call table_products();
 
 select customers.name,
-orders.order_id
+products.product_name
 from novakart.customers as customers
 join novakart.orders as orders
 	on customers.customer_id = orders.customer_id
+join novakart.order_items as items
+	on orders.order_id = items.order_id
+join novakart.products as products
+	on items.product_id = products.product_id
 ;
